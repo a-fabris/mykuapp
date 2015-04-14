a15b91dce62238d6cd544cbe5c9c252ca90dc057
Andrea Fabris <afabris86@gmail.com>
2015-04-14 16:41:45 +0200

    addedd controller

---
+++ inst/www/js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var myKuApp = angular.module('myKuApp',['ui.ace'])
+
+myKuApp.controller('MainCtrl', function($scope){
+	$scope.aceOptions = {
+		theme: 'solarized_dark',
+		mode: 'r',
+		useWrapMode : true
+	}
+	$scope.example = 'code goes here'
+})
