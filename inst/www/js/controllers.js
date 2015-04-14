91dbbd94c403afce6c49d6493263083d417e4367
Andrea Fabris <afabris86@gmail.com>
2015-04-14 17:27:48 +0200

    update

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -5,6 +5,7 @@
 var myKuApp = angular.module('myKuApp',['ui.ace']);
 
 myKuApp.controller('MainCtrl', function ($scope) {
+	$("#output").attr('src', '')
 	$scope.aceOptions = {
 		theme: 'solarized_dark',
 		mode: 'r',
