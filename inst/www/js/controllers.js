104e5dcf96b113199da87452578961199469770f
Andrea Fabris <afabris86@gmail.com>
2015-04-15 18:07:43 +0200

    fixed ace

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -1,24 +1,10 @@
 'use strict';
 
-/* Controllers */
+/*Controllers*/
 
-var myKuApp = angular.module('myKuApp',['ui.ace']);
+var app = angular.module("app",['ui.ace']);
 
-myKuApp.controller('MainCtrl', function ($scope) {
-	
-	$scope.aceOptions = {
-		theme: 'solarized_dark',
-		mode: 'r',
-		useWrapMode : true
-	}
-
-	$scope.functions = [
-		{'name' : 'rnorm', 'description' : 'random generator for the normal distribution'},
-		{'name' : 'dnorm', 'description' : 'this is dnorm'},
-		{'name' : 'qnorm', 'description' : 'this is qnorm'}
-	];
-
-	$scope.example = 'require(rCharts)\nrPlot(mpg ~ wt, data = mtcars,type = "point")'
-
-	$scope.code = "alert('hello world');";
-});
+app.controller("controller", ["$scope", function($scope) {
+		$scope.code = "alert('hello world');";
+		$scope.greeting = "Hola!";
+}]);
