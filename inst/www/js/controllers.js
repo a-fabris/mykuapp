0635ba77b4b81a2610a0b8db6a4a0c29faa615b0
Andrea Fabris <afabris86@gmail.com>
2015-04-15 15:40:34 +0200

    ace editor

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -11,5 +11,20 @@ myKuApp.controller('MainCtrl', function ($scope) {
 		mode: 'r',
 		useWrapMode : true
 	}
+
+	$scope.functions = [
+		{ 	'name' : 'rnorm' , 
+		  	'description' : 'random generator for the normal distribution'},
+		{
+			'name' : 'dnorm',
+			'description' : 'this is dnorm'
+		},
+		{
+			'name' : 'qnorm',
+			'description' : 'this is qnorm'
+		}
+
+	];
+
 	$scope.example = 'code goes here'
 });
