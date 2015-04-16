cee917907f2e4aa4286d7e77be47a2c7947f62fe
Andrea Fabris <afabris86@gmail.com>
2015-04-16 15:26:49 +0200

    fixed path

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module("app",['ui-ace']);
+var app = angular.module("app",['ui.ace']);
 
 app.controller("controller", ["$scope", function($scope) {
 		$scope.code = "alert('hello world');";
