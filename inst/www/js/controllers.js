cb05267f7de563f403efbb6d5470ccfccf02b9c5
Andrea Fabris <afabris86@gmail.com>
2015-04-16 15:44:02 +0200

    fixed path

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -1,6 +1,13 @@
 'use strict';
 
-var app = angular.module("app",['ui.ace']);
+var app2 = angular.module("app2",[]);
+
+app2.controller("controller2", ["$scope", function($scope){
+	$scope.example = "Example works!";
+}]);
+
+
+var app = angular.module("app",['ui.ace','app2']);
 
 app.controller("controller", ["$scope", function($scope) {
 		$scope.code = "alert('hello world');";
@@ -11,9 +18,4 @@ app.controller("controller", ["$scope", function($scope) {
 		}
 }]);
 
-var app2 = angular.module("app2",[]);
-
-app2.controller("controller2", ["$scope", function($scope){
-	$scope.example = "Example works!";
-}]);
 
