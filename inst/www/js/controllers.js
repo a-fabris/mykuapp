e8591cbcc32da51fa908c8e9e9fdfadb7b7e6e84
Andrea Fabris <afabris86@gmail.com>
2015-04-19 17:14:30 +0200

    tests

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -3,7 +3,6 @@
 var app = angular.module("app",['ui.ace']);
 
 app.controller("controller", ["$scope", function($scope) {
-		$scope.code = "alert('hello world');";
 		$scope.aceOptions = {
 			theme : 'solarized_dark',
 			mode : 'r',
@@ -25,6 +24,10 @@ app.controller("controller", ["$scope", function($scope) {
                 default:
                     $scope.code = "function unknown";
             }
+        }
+
+        $scope.runcode = function(code){
+            alert("Running R");     
         }
 
 }]);
