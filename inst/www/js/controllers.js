a3bd2e57d57bdc4fd26d3adc2e675a9b572e149a
Andrea Fabris <afabris86@gmail.com>
2015-04-21 16:19:09 +0200

    ui design

--- inst/www/js/controllers.js
+++ inst/www/js/controllers.js
@@ -19,18 +19,19 @@ app.controller("mainController", ["$scope",  function($scope) {
             {"name" : "tasks"}
         ];
 
-        $scope.menuEntry{
+        $scope.menuEntry{[
             {"name" : "rnorm"},
             {"name" : "mean"},
             {"name" : "standard deviation"}
 
-        }
+        ]};
 
 		$scope.aceOptions = {
 			theme : 'solarized_dark',
 			mode : 'r',
 			useWrapMode : 'true'
-		}
+		};
+
 		$scope.example = "Example Works!";
 
         $scope.makecode = function(funct){
