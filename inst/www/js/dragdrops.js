6b9c1b1b7c5e89bffc1876d3cba4f13995cc9535
Andrea Fabris <afabris86@gmail.com>
2015-04-22 13:08:52 +0200

    dragdrop

--- inst/www/js/dragdrops.js
+++ inst/www/js/dragdrops.js
@@ -3,14 +3,14 @@ $( init );
 function init() {
 
 	$('#workspace').droppable({
-
+		drop : funDropped
 	});
 
   	$('#rnorm').draggable({
   		cursor: 		"move",
   		zIndex: 		100,
   		containment: 	"#workspace",
-  		//helper: 		"clone",
+  		helper: 		"clone",
   		stop: 			rnormstop
   	});
 
@@ -24,4 +24,11 @@ function rnormstop(event, ui){
 	var code = editor.getSession().getValue();
 	// set code
 	editor.getSession().setValue("rnorm(100) \nsource of newline");
+}
+
+function funDropped(event, ui){
+	$("workspace").css({
+		background: '#2B2B2B',
+		color: '#F6F6F6'
+	});
 }
